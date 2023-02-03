7af0dc6df22cab48b6d75b173c5684158ac078b6
Hayyan <hayyan325@gmail.com>
2023-02-03 09:46:07 +0700

    menambahkan method store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -14,6 +15,33 @@ class PostController extends Controller
 
         //return view with data
         return view('posts', compact('posts'));
+    }
+
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create post
+        $post = Post::create([
+            'title'     => $request->title, 
+            'content'   => $request->content
+        ]);
+
+        //return response
+        return response()->json([
+            'success' => true,
+            'message' => 'Data Berhasil Disimpan!',
+            'data'    => $post  
+        ]);
     }
 
 }
